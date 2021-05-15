f615cdc8bd445db39412267064fa9668fcc4d38e
praveen292000 <79620967+praveen292000@users.noreply.github.com>
2021-05-15 18:24:09 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import Nav from "./components/Nav";
 import ProductsPage from "./Pages/ProductPage";
 import Home from "./components/HomePage";
+import StationeryShopPage from "./pages/StationeryShopPage";
 import { Route, Switch } from "react-router";
 
 class App extends React.Component {
@@ -12,7 +13,8 @@ class App extends React.Component {
         <Nav />
         <Switch>
           <Route exact path="/" component={Home} />
-          <Route  path="/product" component={ProductsPage} /> 
+          <Route  path="/product" component={ProductsPage} />
+          <Route  path="/stationeryshop" component={StationeryShopPage} /> 
         </Switch>
       </div>
     );
